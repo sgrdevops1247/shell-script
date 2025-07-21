420d5f7412faa571975d96444664a927735ea676
sgrdevops1247 <sgrdevops1247@gmail.com>
2025-07-21 22:10:31 +0530

    hello script

---
+++ 05-variable.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "please enter username::"
+
+read -s USERNAME #here username is variable
+
+echo "please enter password::"
+
+read -s PASSWORD
+
+echo "Username is: $USERNAME, Password is: $PASSWORD"
