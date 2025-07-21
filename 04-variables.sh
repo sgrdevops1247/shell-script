d92f8f9f2914f3c93423b87ba2f9a4d397ba723f
sgrdevops1247 <sgrdevops1247@gmail.com>
2025-07-21 21:34:50 +0530

    hello script

---
+++ 04-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: hello $PERSON2,how are you?"
+echo "$PERSON2:: Hi $PERSON1,iam fine.How are you?"
+echo "$PERSON1:: I am fine too. How is your work?"
+echo "$PERSON2:: not bad.Iam thinking to upgrade to devops"
