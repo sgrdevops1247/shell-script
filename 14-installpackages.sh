8367bba865682f35cb34b2dfdf036231219c0fdf
sgrdevops1247 <sgrdevops1247@gmail.com>
2025-07-23 20:51:52 +0530

    hello script

--- 14-installpackages.sh
+++ 14-installpackages.sh
@@ -9,6 +9,7 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+VALIDATE(){
    if [ $1 -ne 0 ]
    then
      echo -e "$2...$R FAILURE $N"
@@ -16,7 +17,7 @@ N="\e[0m"
    else
      echo -e "$2...$G SUCCESS $N"
    fi
-
+}
 
 if [ $USERID -ne 0 ]
 then 
